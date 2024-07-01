be56fc9804210583c3953189f695135529bd63ee
Luiz Passos <lzgustavo.passos@gmail.com>
2024-06-30 21:06:24 -0300

    Screen Analyser

--- openai_request.py
+++ openai_request.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 from speech_to_text import SpeechToText
 from tts_response import text_to_speech_file
+from screenshot import ShotAndSave
 import pygame
 import os
+
 
 client = OpenAI()
 
@@ -12,25 +14,42 @@ AI_ROLE = """You are a helpful assistant."""
 def AiTalk():
     user_prompt = SpeechToText()
 
-    print(f'User Prompt: {user_prompt}')
+    if user_prompt.startswith("Capture this."):
+
+        image = ShotAndSave()
+        print(f'User Prompt: {user_prompt}')
+        
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": AI_ROLE},
+                {"role": "user", "content": [{"type" : "text", "text" : user_prompt},
+                                             {"type" : "image_url", "image_url" : {"url": f"data:image/jpeg;base64,{image}"}}]},
+            ]
+        )
+        ai_response = response.choices[0].message.content
+        print(f'\n Ai Response: {ai_response}')
+        
+    else:
+        print(f'User Prompt: {user_prompt}')
+        
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": AI_ROLE},
+                {"role": "user", "content": user_prompt},
+            ]
+        )
+        ai_response = response.choices[0].message.content
+        print(f'\n Ai Response: {ai_response}')
     
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": AI_ROLE},
-            {"role": "user", "content": user_prompt},
-        ]
-    )
-    ai_response = response.choices[0].message.content
-    print(f'\n Ai Response: {ai_response}')
-    last_message = user_prompt
     return ai_response
 
 
 
 if __name__ == "__main__":
     while True:
-        text_to_speech_file(AiTalk())
-    
+        text_to_speech_file(AiTalk())    
+
 
 
---
+++ screenshot.py
@@ -0,0 +1,11 @@
+import pyautogui
+import base64
+
+
+def ShotAndSave():
+    screenshot = pyautogui.screenshot()
+    screenshot.save('screenshot.jpeg')
+    with open('screenshot.jpeg', "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+ShotAndSave()
